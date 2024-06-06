e53118ade896a18594a7597f94ca4d8128fa44d8
Daffa Akhdaan <daaff12@gmail.com>
2024-06-07 06:16:56 +0700

    tinggal fix ui

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const ConfirmationModal = ({ isOpen, closeModal, confirmAction }) => {
+  if (!isOpen) return null;
+
   return (
-    <div className={`modal ${isOpen ? "open" : ""}`}>
+    <div className="modal-overlay">
       <div className="modal-content">
         <h2>Are you sure?</h2>
-        <button onClick={confirmAction}>Yes</button>
-        <button onClick={closeModal}>No</button>
+        <div className="modal-buttons">
+          <button onClick={confirmAction} className="modal-button confirm">
+            Yes
+          </button>
+          <button onClick={closeModal} className="modal-button cancel">
+            No
+          </button>
+        </div>
       </div>
     </div>
   );
